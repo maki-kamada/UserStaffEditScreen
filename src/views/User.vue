da13d19d2f3265ea0500a16119fa8ffe2742f8a6
kamikawa1234 <91867869+kamikawa1234@users.noreply.github.com>
2021-11-22 15:10:17 +0900

    Kamikawa (#2)
    * バリデーションチェック
    
    バリデーションチェックの作成
    
    * バリデーションチェック機能の作成
    
    数値チェック、メール形式チェックなどの機能作成

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,19 +1,70 @@
 <template>
 <div class="container">
-    <!-- <div id="app"> -->
     <form>
-        <!-- 会社名フォーム -->
         <div class="container">
+            <!-- 会社名フォーム -->
             <div>
-                <label for="name">名前</label>
-                <input type="text" v-model="name">
-                <span>{{ errors.name }}</span>
+                <label for="company">会社名</label>
+                <input type="text" v-model="company">
+                <span>{{ errors.company }}</span>
+            </div>
+            <!-- 代表者名フォーム -->
+            <div>
+                <label for="president">代表者名</label>
+                <input type="text" v-model="president">
+                <span>{{ errors.president }}</span>
+            </div>
+            <!-- 郵便番号フォーム -->
+            <div>
+                <label for="zipcode">郵便番号</label>
+                <input type="text" v-model="zipcode1">
+                <input type="text" v-model="zipcode2">
+                <span>{{ errors.zipcode }}</span>
+            </div>
+            <!-- 住所１フォーム -->
+            <div>
+                <label for="prefecture">住所１(都道府県)</label>
+                <input type="text" v-model="prefecture">
+                <span>{{ errors.prefecture }}</span>
+            </div>
+            <!-- 住所２フォーム -->
+            <div>
+                <label for="city">住所２(市区町村)</label>
+                <input type="text" v-model="city">
+                <span>{{ errors.city }}</span>
+            </div>
+            <!-- 住所３フォーム -->
+            <div>
+                <label for="building">住所３(建物名等)</label>
+                <input type="text" v-model="building">
+                <span>{{ errors.building }}</span>
+            </div>
+            <!-- 電話番号フォーム -->
+            <div>
+                <label for="tel">電話番号</label>
+                <input type="text" v-model="tel1">
+                <input type="text" v-model="tel2">
+                <input type="text" v-model="tel3">
+                <span>{{ errors.tel }}</span>
+            </div>
+            <!-- FAXフォーム -->
+            <div>
+                <label for="fax">FAX</label>
+                <input type="text" v-model="fax1">
+                <input type="text" v-model="fax2">
+                <input type="text" v-model="fax3">
+                <span>{{ errors.fax }}</span>
+            </div>
+            <!-- e-mailフォーム -->
+            <div>
+                <label for="mail">e-mail</label>
+                <input type="text" v-model="mail">
+                <span>{{ errors.mail }}</span>
             </div>
         </div>
     </form>
-    <!-- </div> -->
     <div class="register">
-        <button type="button" :disabled="isDisabled">送信</button>
+        <button type="button" :disabled=isDisabled>送信</button>
     </div>
 </div>
 </template>
@@ -25,23 +76,187 @@
 export default {
     data() {
         return {
-            name: '',
+            company: '',
+            president: '',
+            zipcode1: '',
+            zipcode2: '',
+            prefecture: '',
+            city: '',
+            building: '',
+            tel1: '',
+            tel2: '',
+            tel3: '',
+            fax1: '',
+            fax2: '',
+            fax3: '',
+            mail: '',
+            companyFlg: false,
+            zipcodeFlg: false,
+            prefectureFlg: false,
+            cityFlg: false,
+            telFlg: false,
+            mailFlg: false,
             errors: {}
         }
     },
-    computed: {
+    computed: {     // 必須入力項目が未入力の場合、送信ボタンが非活性化する
         isDisabled() {
-            return Object.keys(this.errors).length > 0 ? true : false
+            return this.companyFlg == true && this.zipcodeFlg == true && this.prefectureFlg == true && this.cityFlg == true && this.telFlg == true && this.mailFlg == true && Object.keys(this.errors).length == 0 ? false : true
         }
     },
-    watch: {
-        name(name) {
-            if (name) {
-                this.$delete(this.errors, 'name')
+    watch: {        // リアルタイムでバリデーションチェックを行う
+        company(company) {
+            if (!company) {
+                this.$set(this.errors, "company", '必須入力項目です。')
+                this.companyFlg = false;
+            } else if (company.length > 50) {
+                this.$set(this.errors, "company", '50文字以内で入力してください。')
+                this.companyFlg = false;
             } else {
-                this.$set(this.errors, "name", '必須入力項目です。')
+                this.$delete(this.errors, 'company')
+                this.companyFlg = true;
             }
-        }
+        },
+        president(president) {
+            if (president.length > 50) {
+                this.$set(this.errors, "president", '50文字以内で入力してください。')
+            } else {
+                this.$delete(this.errors, 'president')
+            }
+        },
+        zipcode1(zipcode1) {
+            if (!zipcode1 || !this.zipcode2) {
+                this.$set(this.errors, "zipcode", '必須入力項目です。')
+                this.zipcodeFlg = false;
+            } else if (zipcode1.match(/^\d{3}$/) && this.zipcode2.match(/^\d{4}$/)) {
+                this.$delete(this.errors, 'zipcode')
+                this.zipcodeFlg = true;
+            } else {
+                this.$set(this.errors, "zipcode", '半角数字で入力してください。（3桁＋4桁）')
+                this.zipcodeFlg = false;
+            }
+        },
+        zipcode2(zipcode2) {
+            if (!this.zipcode1 || !zipcode2) {
+                this.$set(this.errors, "zipcode", '必須入力項目です。')
+                this.zipcodeFlg = false;
+            } else if (this.zipcode1.match(/^\d{3}$/) && zipcode2.match(/^\d{4}$/)) {
+                this.$delete(this.errors, 'zipcode')
+                this.zipcodeFlg = true;
+            } else {
+                this.$set(this.errors, "zipcode", '半角数字で入力してください。（3桁＋4桁）')
+                this.zipcodeFlg = false;
+            }
+        },
+        prefecture(prefecture) {
+            if (!prefecture) {
+                this.$set(this.errors, "prefecture", '必須入力項目です。')
+                this.prefectureFlg = false;
+            } else if (prefecture.length > 50) {
+                this.$set(this.errors, "president", '50文字以内で入力してください。')
+                this.prefectureFlg = false;
+            } else {
+                this.$delete(this.errors, 'prefecture')
+                this.prefectureFlg = true;
+            }
+        },
+        city(city) {
+            if (!city) {
+                this.$set(this.errors, "city", '必須入力項目です。')
+                this.cityFlg = false;
+            } else if (city.length > 50) {
+                this.$set(this.errors, "city", '50文字以内で入力してください。')
+                this.cityFlg = false;
+            } else {
+                this.$delete(this.errors, 'city')
+                this.cityFlg = true;
+            }
+        },
+        building(building) {
+            if (building.length > 50) {
+                this.$set(this.errors, "building", '50文字以内で入力してください。')
+            } else {
+                this.$delete(this.errors, 'building')
+            }
+        },
+        tel1(tel1) {
+            if (!tel1 || !this.tel2 || !this.tel3) {
+                this.$set(this.errors, "tel", '必須入力項目です。')
+                this.telFlg = false;
+            } else if (tel1.match(/^\d{2,3}$/) && this.tel2.match(/^\d{1,4}$/) && this.tel3.match(/^\d{4}$/)) {
+                this.$delete(this.errors, 'tel')
+                this.telFlg = true;
+            } else {
+                this.$set(this.errors, "tel", '半角数字で入力してください。（2~3桁＋1~4桁＋4桁）')
+                this.telFlg = false;
+            }
+        },
+        tel2(tel2) {
+            if (!this.tel1 || !tel2 || !this.tel3) {
+                this.$set(this.errors, "tel", '必須入力項目です。')
+                this.telFlg = false;
+            } else if (this.tel1.match(/^\d{2,3}$/) && tel2.match(/^\d{1,4}$/) && this.tel3.match(/^\d{4}$/)) {
+                this.$delete(this.errors, 'tel')
+                this.telFlg = true;
+            } else {
+                this.$set(this.errors, "tel", '半角数字で入力してください。（2~3桁＋1~4桁＋4桁）')
+                this.telFlg = false;
+            }
+        },
+        tel3(tel3) {
+            if (!this.tel1 || !this.tel2 || !tel3) {
+                this.$set(this.errors, "tel", '必須入力項目です。')
+                this.telFlg = false;
+            } else if (this.tel1.match(/^\d{2,3}$/) && this.tel2.match(/^\d{1,4}$/) && tel3.match(/^\d{4}$/)) {
+                this.$delete(this.errors, 'tel')
+                this.telFlg = true;
+            } else {
+                this.$set(this.errors, "tel", '半角数字で入力してください。（2~3桁＋1~4桁＋4桁）')
+                this.telFlg = false;
+            }
+        },
+        fax1(fax1) {
+            if (fax1 == "" && this.fax2 == "" && this.fax3 == "") {
+                this.$delete(this.errors, 'fax')
+            } else if (!fax1.match(/^\d{2,3}$/) || !this.fax2.match(/^\d{1,4}$/) || !this.fax3.match(/^\d{4}$/)) {
+                this.$set(this.errors, "fax", '半角数字で入力してください。（2~3桁＋1~4桁＋4桁）')
+            } else {
+                this.$delete(this.errors, 'fax')
+            }
+        },
+        fax2(fax2) {
+            if (this.fax1 == "" && fax2 == "" && this.fax3 == "") {
+                this.$delete(this.errors, 'fax')
+            } else if (!this.fax1.match(/^\d{2,3}$/) || fax2.match(/^\d{1,4}$/) || !this.fax3.match(/^\d{4}$/)) {
+                this.$set(this.errors, "fax", '半角数字で入力してください。（2~3桁＋1~4桁＋4桁）')
+            } else {
+                this.$delete(this.errors, 'fax')
+            }
+        },
+        fax3(fax3) {
+            if (this.fax1 == "" && this.fax2 == "" && fax3 == "") {
+                this.$delete(this.errors, 'fax')
+            } else if (!this.fax1.match(/^\d{2,3}$/) || !this.fax2.match(/^\d{1,4}$/) || !fax3.match(/^\d{4}$/)) {
+                this.$set(this.errors, "fax", '半角数字で入力してください。（2~3桁＋1~4桁＋4桁）')
+            } else {
+                this.$delete(this.errors, 'fax')
+            }
+        },
+        mail(mail) {
+            if (!mail) {
+                this.$set(this.errors, "mail", '必須入力項目です。')
+                this.mailFlg = false;
+            } else if (!mail.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+                this.$set(this.errors, "mail", '正しいメール形式で入力してください。')
+                this.mailFlg = false;
+            } else if (mail.length > 40) {
+                this.$set(this.errors, "mail", '40文字以内で入力してください。')
+                this.mailFlg = false;
+            } else {
+                this.$delete(this.errors, 'mail')
+                this.mailFlg = true;
+            }
+        },
     }
 };
 </script>
